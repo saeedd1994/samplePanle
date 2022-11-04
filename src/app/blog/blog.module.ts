0791dd9d513c9980fac33ddff28ae8e8274b007f
saeed <saeed.da1994@gmail.com>
2022-11-04 22:18:48 +0330

    stable state => add blog-list component

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BlogRoutingModule } from './blog-routing.module';
+import { BlogListComponent } from './blog-list/blog-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BlogListComponent
+  ],
   imports: [
     CommonModule,
     BlogRoutingModule
