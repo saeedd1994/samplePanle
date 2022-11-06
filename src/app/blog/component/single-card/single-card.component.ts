b468e2e9f29d54b352c96d41fd718c87a74c1d91
saeed <saeed.da1994@gmail.com>
2022-11-06 22:35:35 +0330

    stable state => implement single card and use ngFor and @input for relation

--- src/app/blog/component/single-card/single-card.component.ts
+++ src/app/blog/component/single-card/single-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-single-card',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SingleCardComponent implements OnInit {
 
-  constructor() { }
+  @Input() title!: string;
+  @Input() content!: string;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
--- src/app/blog/pages/blog-list/blog-list.component.ts
+++ src/app/blog/pages/blog-list/blog-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-blog-list',
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogListComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
+
+  blogs = [
+    {
+      title: 'first blog',
+      content: 'One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiffOne morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff'
+    },
+    {
+      title: 'second blog',
+      content: 'One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff'
+    },
+    {
+      title: 'third blog',
+      content: 'One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff'
+    }
+  ]
 
 }
